97f437a26fe6141fc584e937f4d702889a233ad4
Ryan T <memopemomemo@gmail.com>
2023-09-19 12:00:18 -0400

    Fixed movement bugs

--- block-dupe-project/Assets/Scripts/Player.cs
+++ block-dupe-project/Assets/Scripts/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour
         };
     
     [SerializeField] LayerMask ground;
+    [SerializeField] LayerMask wall;
     [SerializeField] int jumpHeight;
     [SerializeField] int runSpeed;
     [SerializeField] int maxSpeed;
@@ -39,8 +40,8 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rigidBody.AddForce(Vector2.up * jumpHeight * 20f, ForceMode2D.Impulse);
-
         }
+        
         print(
             "B" + (IsGrounded() ? "1 " : "0 ") +
             "L" + (IsTouchingLeftWall() ? "1 " : "0 ") +
@@ -50,7 +51,7 @@ public class Player : MonoBehaviour
 
         //Animation, (Yes, I should probably make a state machine)
         
-        if(Input.GetAxisRaw("Vertical") < 0 && IsGrounded())
+        if((Input.GetAxisRaw("Vertical") < 0 && IsGrounded()) || (IsGrounded() && IsTouchingCeiling() && Mathf.Abs(rigidBody.velocity.y) < 0.2f ))
         {
             GetComponent<BoxCollider2D>().size = Vector2.one * 0.95f;
             GetComponent<BoxCollider2D>().offset = Vector2.down * 0.45f;
@@ -95,7 +96,7 @@ public class Player : MonoBehaviour
         }
         if(Input.GetAxisRaw("Horizontal") != 0)
         {
-            if(IsTouchingRightWall() && direction|| IsTouchingLeftWall() && !direction)
+            if(IsTouchingRightWall() && direction || IsTouchingLeftWall() && !direction)
             {
                 animator2D.SetAnimation(State.WallRun);
             }
@@ -154,25 +155,25 @@ public class Player : MonoBehaviour
     }
     public bool IsGrounded()
     {
-        var col = Physics2D.BoxCast(transform.position+Vector3.down*0.125f, GetComponent<Collider2D>().bounds.size, 0, Vector2.down, 0.5f, ground);
-        if(col) Debug.DrawLine(transform.position,col.point);
+        var col = Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.down, 0.5f, ground);
+        if(col) Debug.DrawLine((Vector2)transform.position + GetComponent<Collider2D>().offset, col.point, Color.magenta);
         return col;
     }
     public bool IsTouchingLeftWall()
     {
-        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.left, 0.1f, ground);
+        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.left, 0.1f, wall);
     }
     public bool IsTouchingRightWall()
     {
-        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.right, 0.1f, ground);
+        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.right, 0.1f, wall);
     }
     public bool IsTouchingCeiling()
     {
-        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.up, 0.1f, ground);
+        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset, GetComponent<Collider2D>().bounds.size, 0, Vector2.up, 0.1f, wall);
     }
     public bool HasSpaceToLift(BoxCollider2D collider)
     {
-        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset  + Vector2.up, collider.bounds.size, 0, Vector2.zero, 0, ground);
+        return Physics2D.BoxCast((Vector2)transform.position + GetComponent<Collider2D>().offset  + Vector2.up, collider.bounds.size, 0, Vector2.zero, 0, wall);
     }
  
     //Drawing Box Checks
